5d20ac8b8d996886b97b678c6d265c1420b21460
LordFrz <lfearctic@yahoo.com>
2017-09-06 20:08:59 -0500

    Added Battles

--- AKBot.js
+++ AKBot.js
@@ -8,13 +8,14 @@ const Albion = require('albion-api');
 const Discord = require('discord.io');
 const fs = require('fs');
 const logger = require('winston');
-
+const request = require('request');
 const auth = require('./auth.json');
 const config = require('./config.json');
 
 // Global list of used kill IDs
 // Will eventually be switched to a database
 let killList;
+let battleList;
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -26,6 +27,11 @@ logger.level = 'debug';
 fs.readFile('eventID.json', 'utf8', (err, data) => {
   if (err) { throw err; }
   killList = JSON.parse(data);
+});
+
+fs.readFile('battleID.json', 'utf8', (err, data) => {
+  if (err) { throw err; }
+  battleList = JSON.parse(data);
 });
 
 // Initialize Discord Bot
@@ -43,73 +49,72 @@ bot.on('ready', () => {
 
   //Runs Kill post function every 60seconds
   setInterval(checkKillboard, 60000);
+  
+  
 });
-
-function checkKillboard() {
-  Albion.getRecentEvents({ limit: 51, offset: 0 }, (err, events) => {
-    events.forEach(event => {
-      const eventID = event.EventId;
-      if (event.Killer.AllianceName !== config.AllianceTAG
-          || killList.kills.indexOf(eventID) !== -1) {
-        return;
-      }
-
-      killList.kills.push(eventID);
-      fs.writeFile('eventID.json', JSON.stringify(killList), 'utf8', (err) => {
-        if (err) { logger.error(`Error writing to file: ${err}`) }
-      });
-
-      Albion.getEventDetails(eventID, (err, event) => {
-        const weapon = event.Killer.Equipment.MainHand;
-        const imgUrl = [
-          `https://gameinfo.albiononline.com/api/gameinfo/items/`,
-          `${weapon.Type}.png`,
-          `?count=${weapon.Count}`,
-          `&quality=${weapon.Quality}`
-        ].join('');
-
-        const participants = parseInt(event.numberOfParticipants, 10);
-        let assists = participants - 1;
-        if (assists < 0)
-        {
-          assists = 0;
-        }
-        
-        let killerDamage;
-        const totalDamage = event.Participants.reduce((damage, participant) => {
-          if (participant.Name === event.Killer.Name) {
-            killerDamage = participant.DamageDone;
-          }
-          return damage + participant.DamageDone;
-        }, 0);
-
-        let killerPercent = Math.round((killerDamage / totalDamage) * 100);
-        if (killerPercent == "NaN")
-        {
-          killerPercent = 100;
-        }
-        
-        bot.sendMessage({
-          to: config.Channel,
-          embed: {
-            title: `${event.Killer.Name} of ${event.Killer.GuildName}`,
-            description: [
-              `**Victim**: ${event.Victim.Name}`,
-              `**KillFame**: ${event.TotalVictimKillFame}`,
-              `**VictimAlliance**: ${event.Victim.AllianceName}`,
-              `**VictimGuild**: ${event.Victim.GuildName}`,
-              `**DamageDone**: ${killerPercent}%`,
-              `**Assists**: ${assists}`,
-            ].join('\n'),
-            url: `https://albiononline.com/en/killboard/kill/${event.EventId}`,
-            thumbnail: { url: imgUrl },
-          }
-        });
-      })
-    });
+function urlRequest(uri, cb) {
+var url = `https://gameinfo.albiononline.com/api/gameinfo/${uri}`;
+  request(url, function (error, response, body) {
+    cb(null, JSON.parse(body));
   });
 }
 
+
+function checkKillboard() {
+	Albion.getRecentEvents({ limit: 51, offset: 0 }, (err, events) => {
+
+	events.forEach(event => {
+	  const eventID = event.EventId;
+	  if (event.Killer.AllianceName !== config.AllianceTAG
+		  || killList.kills.indexOf(eventID) !== -1) {
+		return;
+	  }
+
+	  killList.kills.push(eventID);
+	  fs.writeFile('eventID.json', JSON.stringify(killList), 'utf8', (err) => {
+		if (err) { logger.error(`Error writing to file: ${err}`) }
+	  });
+	  //Run the post event function
+	  postEvent(eventID);
+	  
+	});
+	});
+  //Check the battle board for battles
+	urlRequest(`battles?limit=51&sort=recent`, (err, battles) => {
+		battles.forEach(battle => {
+			const battleID = battle.id;
+			if(battleList.battles.indexOf(battleID) !== -1
+				|| battle.alliances.hasOwnProperty(config.AllianceKey)!== true)
+			{
+				return;
+			}
+			battleList.battles.push(battleID);
+			fs.writeFile('battleID.json', JSON.stringify(battleList), 'utf8', (err) => {
+				if (err) { logger.error(`Error writing to file: ${err}`) }
+			});
+			postBattle(battleID);
+		});
+	});
+}
+function postBattle(battleID)
+{
+	urlRequest(`battles/${battleID}`, (err, battle) => {
+		const tag = config.AllianceKey;
+        bot.sendMessage({
+          to: config.Channel,
+          embed: {
+            title: `Battle Report`,
+            description: [
+              `**Kills**: ${battle.alliances[tag].kills}`,
+              `**Deaths**: ${battle.alliances[tag].deaths}`,
+               `**KillFame**: ${battle.alliances[tag].killFame}`,
+            ].join('\n'),
+            url: `https://albiononline.com/en/killboard/battles/${battleID}`,
+          }
+        });
+			
+	});
+}
 bot.on('message', (user, userID, channelID, message) => {
   if (message.substring(0, 1) !== '!') { return; }
 
@@ -127,19 +132,16 @@ bot.on('message', (user, userID, channelID, message) => {
         });
       });
       break;
-    case 'akCmd':
-    case 'akCommands':
     case 'akHelp':
       bot.sendMessage({
         to: channelID,
         message: [
           'List of commands',
           '----------------',
-          '!akCmd - List commands',
-          '!akCommands - List commands',
           '!akHelp - List commands',
           '!akPurgeList - Clears list of all posted events',
           '!akStatus - Show server status',
+		  '!akPK (kill ID) - Show specific kill. Ex(!akPK 9876543)'
         ].join('\n')
       });
       break;
@@ -151,5 +153,65 @@ bot.on('message', (user, userID, channelID, message) => {
         message: 'All EventIDs cleared from file.'
       });
       break;
+	  case 'akPK':
+		const eventID = args[0];
+		postEvent(eventID);
+	  
+	  break;
   }
 });
+function postEvent(eventID)
+{
+      Albion.getEventDetails(eventID, (err, event) => {
+        const weapon = event.Killer.Equipment.MainHand;
+        const imgUrl = [
+          `https://gameinfo.albiononline.com/api/gameinfo/items/`,
+          `${weapon.Type}.png`,
+          `?count=${weapon.Count}`,
+          `&quality=${weapon.Quality}`
+        ].join('');
+
+        const participants = parseInt(event.numberOfParticipants, 10);
+        const assists = participants - 1;
+		if (assists < 0)
+		{
+			assists = 0;
+		}
+		
+        let killerDamage;
+        const totalDamage = event.Participants.reduce((damage, participant) => {
+          if (participant.Name === event.Killer.Name) {
+            killerDamage = participant.DamageDone;
+          }
+          return damage + participant.DamageDone;
+        }, 0);
+
+        const killerPercent = Math.round((killerDamage / totalDamage) * 100);
+		if (killerPercent == "NaN")
+		{
+			killerPercent = 100;
+		}
+		
+		
+        bot.sendMessage({
+          to: config.Channel,
+          embed: {
+            title: `${event.Killer.Name} of ${event.Killer.GuildName}`,
+            description: [
+              `**Victim**: ${event.Victim.Name}`,
+              `**KillFame**: ${event.TotalVictimKillFame}`,
+              `**VictimAlliance**: ${event.Victim.AllianceName}`,
+              `**VictimGuild**: ${event.Victim.GuildName}`,
+              `**DamageDone**: ${killerPercent}%`,
+              `**Assists**: ${assists}`,
+            ].join('\n'),
+            url: `https://albiononline.com/en/killboard/kill/${event.EventId}`,
+            thumbnail: { url: imgUrl },
+			//fields: [{
+						//name: "Healer",
+						//value: "Healer and their stats go here."
+				    //}],
+          }
+        });
+      });
+}
